3d88aa5354edbd2da937103edf98e4ed029e3e24
monkeycs60 <clement.serizay@gmail.com>
2022-03-20 16:57:46 +0100

    update

---
+++ script.js
@@ -0,0 +1,42 @@
+var findDigit = function(num, nth){
+    
+    let resultat = Array.from(String(num), num => Number(num));
+    index = (resultat.length - nth);
+    console.log(index);
+   
+    
+    
+    
+   
+        if (num == 0) {
+            return 0;
+
+        }
+        else if (nth <= 0) {
+return -1;
+        }
+        else if( index <= -0.5 ) {
+            return 0;  
+        }
+        else if (index <= -1) {
+            return -1;
+        }
+    else {
+        
+        let finalResult = resultat[index]
+        return finalResult;
+    }
+    
+   
+}
+
+console.log(findDigit(5673, 4));
+
+
+// 5673, 4 --> 5
+// 129, 2 --> 2
+// -2825, 3 --> 8
+// -456, 4 --> 0
+// 0, 20 --> 0
+// 65, 0 --> -1
+// 24, -8 --> -1
